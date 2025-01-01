4c0b9d2118f1013be9f5defb287740a95aa68eed
Yuvraj Singh Nain <yuvrajsinghnain03@gmail.com>
2025-01-01 13:41:14 +0530

    Adding go backend

---
+++ main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"strings"
+
+	"github.com/gorilla/mux"
+)
+
+type DomainURL struct {
+	DomainURL string `string:"domainurl"`
+}
+
+type DomainVar struct {
+	Domain      string `json:"domain"`
+	HasMX       bool   `json:"hasMX"`
+	HasSPF      bool   `json:"hasSPF"`
+	SPFRecord   string `json:"spfRecord"`
+	HasDMARC    bool   `json:"hasDMARC"`
+	DMARCRecord string `json:"dmarcRecord"`
+}
+
+var domainVars []DomainVar
+
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var domainUrl DomainURL
+	json.NewDecoder(r.Body).Decode(&domainUrl)
+	domainVar := isValidDomain(domainUrl.DomainURL)
+	domainVars = append(domainVars, domainVar)
+	json.NewEncoder(w).Encode(domainVars)
+}
+
+func isValidDomain(domain string) DomainVar {
+	var hasMX, hasSPF, hasDMARC bool
+	var spfRecord, dmarcRecord string
+	mxRecords, err := net.LookupMX(domain)
+	if err != nil {
+		log.Printf("Error: %v\n", err)
+	}
+	if len(mxRecords) > 0 {
+		hasMX = true
+	}
+	txtRecords, err := net.LookupTXT(domain)
+	if err != nil {
+		log.Printf("Error: %v\n", err)
+	}
+	for _, record := range txtRecords {
+		if strings.HasPrefix(record, "v=spf1") {
+			hasSPF = true
+			spfRecord = record
+			break
+		}
+	}
+	dmarcRecords, err := net.LookupTXT("_dmarc." + domain)
+	if err != nil {
+		log.Printf("Error: %v\n", err)
+	}
+	for _, record := range dmarcRecords {
+		if strings.HasPrefix(record, "v=DMARC1") {
+			hasDMARC = true
+			dmarcRecord = record
+			break
+		}
+	}
+	fmt.Printf("domain=%v\n,hasMX=%v\n,hasSPF=%v\n,spfRecord=%v\n,hasDMARC=%v\n,dmarcRecord=%v\n", domain, hasMX, hasSPF, spfRecord, hasDMARC, dmarcRecord)
+	var domainVar DomainVar
+	domainVar.Domain = domain
+	domainVar.HasMX = hasMX
+	domainVar.HasSPF = hasSPF
+	domainVar.SPFRecord = spfRecord
+	domainVar.HasDMARC = hasDMARC
+	domainVar.DMARCRecord = dmarcRecord
+
+	return domainVar
+}
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/form", formHandler).Methods("POST")
+	fmt.Print("Server is running on port 8080\n")
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
